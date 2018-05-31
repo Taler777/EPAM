c735f00657afc05655fb86b9e3f63bb44286df59
Alexey <taler2002@gmail.com>
2018-06-01 01:40:44 +0300

    intro hometask 5

---
+++ src/hometask01/Task4.java
@@ -0,0 +1,30 @@
+package hometask01;
+/*
+Даны действительные числа a1, a2, ..., a2n.
+Найти max (a1 + a2n , a2 + a2n – 1, ... , an + an+1 );
+ */
+
+import java.util.Random;
+
+public class Task4 {
+    static int n = 10;
+    static int size = 2 * n + 1;
+    static int[] a = new int[size];
+    static Random random = new Random();
+    static int max;
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < size; i++) {
+            a[i] = random.nextInt(10);
+            System.out.print(a[i] + "  ");
+        }
+        System.out.println();
+        max = a[1] + a[2 * n];
+        for (int i = 1; i <= n; i++) {
+            System.out.println("a["+i+"] = " + a[i] + "    a["+(2 * n + 1 - i)+"] = " + a[2 * n + 1 - i] + "     Сумма = " + (a[i] + a[2 * n + 1 - i]));
+            if (max < a[i] + a[2 * n + 1 - i]) max = a[i] + a[2 * n + 1 - i];
+        }
+        System.out.println("max = " + max);
+    }
+}
